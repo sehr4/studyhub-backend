ac46f2174b5fd764c325678b306f731ac1c15baf
Lidizz <265956@usn.no>
2025-04-06 00:51:45 +0200

    added methods to course service and controller

--- src/main/java/com/studyhub/controller/CourseController.java
+++ src/main/java/com/studyhub/controller/CourseController.java
@@ -3,13 +3,13 @@ package com.studyhub.controller;
 
 import com.studyhub.dto.CourseCreateDTO;
 import com.studyhub.dto.CourseDTO;
+import com.studyhub.dto.EnrollmentDTO;
 import com.studyhub.service.CourseService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 // REST controller for handling course-related API requests
@@ -27,5 +27,23 @@ public class CourseController {
     public ResponseEntity<CourseDTO> createCourse(@Valid @RequestBody CourseCreateDTO courseCreateDTO) {
         CourseDTO courseDTO = courseService.createCourse(courseCreateDTO);
         return ResponseEntity.ok(courseDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable Long id) {
+        CourseDTO courseDTO = courseService.getCourseById(id);
+        return ResponseEntity.ok(courseDTO);
+    }
+
+    @PostMapping("/enroll")
+    public ResponseEntity<Void> enrollStudent(@Valid @RequestBody EnrollmentDTO enrollmentDTO) {
+        courseService.enrollStudent(enrollmentDTO);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/department/{depratment}")
+    public ResponseEntity<List<CourseDTO>> getCoursesByDepartment(@PathVariable String depratment) {
+        List<CourseDTO> courses = courseService.getCourseByDepartment(depratment);
+        return ResponseEntity.ok(courses);
     }
 }
--- src/main/java/com/studyhub/service/CourseService.java
+++ src/main/java/com/studyhub/service/CourseService.java
@@ -4,6 +4,7 @@ package com.studyhub.service;
 import com.studyhub.constant.RoleConstant;
 import com.studyhub.dto.CourseCreateDTO;
 import com.studyhub.dto.CourseDTO;
+import com.studyhub.dto.EnrollmentDTO;
 import com.studyhub.exception.BadRequestException;
 import com.studyhub.exception.ResourceNotFoundException;
 import com.studyhub.mapper.CourseMapper;
@@ -14,7 +15,9 @@ import com.studyhub.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -59,5 +62,37 @@ public class CourseService {
         // Save and return DTO
         Course savedCourse = courseRepository.save(course);
         return courseMapper.toDTO(savedCourse);
+    }
+
+    public CourseDTO getCourseById(Long id) {
+        Course course = courseRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Course with ID: " + id + " not found"));
+        return courseMapper.toDTO(course);
+    }
+
+    public void enrollStudent(EnrollmentDTO enrollmentDTO) {
+        Course course = courseRepository.findById(enrollmentDTO.getCourseId())
+                .orElseThrow(() -> new ResourceNotFoundException("Course with ID: " + enrollmentDTO.getCourseId() + "was not found"));
+        User student = userRepository.findUserById(enrollmentDTO.getStudentId())
+                .orElseThrow(() -> new ResourceNotFoundException("Student with ID: " + enrollmentDTO.getStudentId() + "was not found"));
+
+        if (!student.getRole().equals(RoleConstant.STUDENT)) {
+            throw new BadRequestException("User: " + enrollmentDTO.getStudentId() + " is not a student");
+        }
+        course.getStudents().add(student);
+        courseRepository.save(course);
+    }
+
+    public List<CourseDTO> getCourseByDepartment(String department) {
+        List<Course> courses = courseRepository.findCoursesByDepartment(department);
+        if (courses.isEmpty()) {
+            throw new ResourceNotFoundException("No courses found in department: " + department);
+        }
+        // Convert each course to CourseDTO
+        List<CourseDTO> courseDTOs = new ArrayList<>();
+        for (Course course : courses) {
+            courseDTOs.add(courseMapper.toDTO(course));
+        }
+        return courseDTOs;
     }
 }
