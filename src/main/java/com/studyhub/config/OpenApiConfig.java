98083ac5868c893185d241519e80bd52f8c83228
Lidizz <265956@usn.no>
2025-04-08 18:33:06 +0200

    addad an OpenApiConfig class wite temporary information

--- src/main/java/com/studyhub/config/OpenApiConfig.java
+++ src/main/java/com/studyhub/config/OpenApiConfig.java
@@ -1,4 +1,16 @@
 package com.studyhub.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI studyhubOpenAPI() {
+        return new OpenAPI().info(new Info().title("StudyHub RESTful API").description("My StudyHub service").version("Version 1.1"));
+    }
 }
