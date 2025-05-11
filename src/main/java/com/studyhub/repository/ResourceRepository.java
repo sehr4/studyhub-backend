813621842adf5a7fd87025b0c04fd9683a7673d1
Lidizz <265956@usn.no>
2025-05-11 21:39:28 +0200

    added ResourceRepository

---
+++ src/main/java/com/studyhub/repository/ResourceRepository.java
@@ -0,0 +1,12 @@
+package com.studyhub.repository;
+
+import com.studyhub.model.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ResourceRepository extends JpaRepository<Resource, Long> {
+    List<Resource> findByModuleId(Long moduleId);
+}
